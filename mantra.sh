84c50326d19b32906f60180219b7c6ca6155c760
Herman Ciechanowiec <herman@ciechanowiec.eu>
2022-03-18 22:18:37 +0100

    Make test public

--- mantra.sh
+++ mantra.sh
@@ -104,7 +104,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class MainTest {
+public class MainTest {
 
     @Test
     public void sampleTrueTest() {

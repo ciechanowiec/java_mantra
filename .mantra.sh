67f1aa6b026a5ac6fe58d062ea22467eb65625b4
Herman Ciechanowiec <herman@ciechanowiec.eu>
2022-05-22 17:22:56 +0200

    Enhance script

--- .mantra.sh
+++ .mantra.sh
@@ -90,7 +90,9 @@ secondLevelPackageName=$3
 projectName=$4
 	mkdir -p $1/src/{main/{java/$firstLevelPackageName/$secondLevelPackageName/$projectName,resources},test/java/$firstLevelPackageName/$secondLevelPackageName/$projectName}
 	touch $1/src/main/java/$firstLevelPackageName/$secondLevelPackageName/$projectName/Main.java
+	touch $1/src/main/java/$firstLevelPackageName/$secondLevelPackageName/$projectName/SamplePrinter.java
 	touch $1/src/main/resources/tinylog.properties
+	touch $1/src/main/resources/sampleLines.txt
 	touch $1/src/test/java/$firstLevelPackageName/$secondLevelPackageName/$projectName/MainTest.java
 	touch $1/pom.xml
 	touch $1/README.md
@@ -117,19 +119,93 @@ import org.tinylog.Logger;
 class Main {
 
     public static void main(String[] args) {
-        Logger.info("Application started.");
+        Logger.info("Application started");
         System.out.println("Hello, Universe!");
-        Logger.info("Application ended.");
+
+        Logger.info("Testing resource printing...");
+        SamplePrinter samplePrinter = new SamplePrinter();
+        samplePrinter.performSamplePrint("sample.txt");
+        Logger.info("Finished resource printing");
+
+        Logger.info("Application ended");
     }
 }		
 EOF
 printf "\e[1;96m[STATUS]:\e[0m Default Java-content has been added to \e[3mMain.java\e[0m.\n" 
 }
 
+insertContentToSamplePrinter () {
+projectDirectory=$1
+firstLevelPackageName=$2
+secondLevelPackageName=$3
+projectName=$4
+gitCommitterName=$5
+gitCommitterSurname=$6
+mainFile=$projectDirectory/src/main/java/$firstLevelPackageName/$secondLevelPackageName/$projectName/Main.java
+cat > $mainFile << EOF
+package $firstLevelPackageName.$secondLevelPackageName.$projectName;
+
+import org.tinylog.Logger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author $gitCommitterName $gitCommitterSurname
+ */
+class SamplePrinter {
+
+    void performSamplePrint(String fileName) {
+        InputStream fileFromResourcesAsStream = getFileFromResourcesAsStream(fileName);
+        printInputStream(fileFromResourcesAsStream);
+    }
+
+    private InputStream getFileFromResourcesAsStream(String fileName) {
+        Class<? extends SamplePrinter> samplePrinterClass = this.getClass();
+        ClassLoader classLoader = samplePrinterClass.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+        if (inputStream == null) {
+            throw new IllegalArgumentException(String.format("File '%s' wasn't found!", fileName));
+        } else {
+            return inputStream;
+        }
+    }
+
+    private static void printInputStream(InputStream inputStream) {
+        try (InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+             BufferedReader reader = new BufferedReader(streamReader)) {
+            String line = reader.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = reader.readLine();
+            }
+        } catch (IOException exception) {
+            Logger.error(exception);
+        }
+    }
+}
+EOF
+printf "\e[1;96m[STATUS]:\e[0m Default Java-content has been added to \e[3mSamplePrinter.java\e[0m.\n"
+}
+
+insertContentToSampleLines () {
+sampleLinesFile=$1/src/main/resources/sampleLines.txt
+cat > $sampleLinesFile << EOF
+This is the first line from a sample file.
+This is the second line from a sample file.
+EOF
+printf "\e[1;96m[STATUS]:\e[0m Default logger properties have been added to \e[3msampleLines.txt\e[0m.\n"
+}
+
 insertContentToLoggerProperties () {
 loggerPropertiesFile=$1/src/main/resources/tinylog.properties
 cat > $loggerPropertiesFile << EOF
-writer        = file
+writer        = console
+# to write to a file:
+# writer        = file
 writer.format = {date: yyyy-MM-dd HH:mm:ss.SSS O} {level}: {message}
 writer.file   = logs.txt
 EOF
@@ -184,121 +260,92 @@ cat > $pomFile << EOF
   <url>$projectURL</url>
 
   <properties>
+    <!--  building properties  -->
     <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
     <maven.compiler.release>17</maven.compiler.release>
-    <testng.version>7.5</testng.version>
+    <!--  dependencies  -->
+    <testng.version>7.6.0</testng.version>
     <tinylog-api.version>2.5.0-M1.1</tinylog-api.version>
     <tinylog-impl.version>2.5.0-M1.1</tinylog-impl.version>
+    <!-- plugins -->
     <maven-compiler-plugin.version>3.10.1</maven-compiler-plugin.version>
-    <maven-resources-plugin.version>3.2.0</maven-resources-plugin.version>
-    <maven-jar-plugin.version>3.2.2</maven-jar-plugin.version>
-    <maven-dependency-plugin.version>3.3.0</maven-dependency-plugin.version>
-    <maven-surefire-plugin.version>3.0.0-M5</maven-surefire-plugin.version>
-    <maven-failsafe-plugin.version>3.0.0-M5</maven-failsafe-plugin.version>
-    <jacoco-maven-plugin.version>0.8.7</jacoco-maven-plugin.version>
+    <maven-shade-plugin.version>3.3.0</maven-shade-plugin.version>
+    <maven-surefire-plugin.version>3.0.0-M6</maven-surefire-plugin.version>
+    <maven-failsafe-plugin.version>3.0.0-M6</maven-failsafe-plugin.version>
+    <jacoco-maven-plugin.version>0.8.8</jacoco-maven-plugin.version>
   </properties>
 
   <dependencies>
     <dependency>
       <groupId>org.testng</groupId>
       <artifactId>testng</artifactId>
-      <version>\${testng.version}</version>
+      <version>${testng.version}</version>
       <scope>test</scope>
     </dependency>
     <dependency>
       <groupId>org.tinylog</groupId>
       <artifactId>tinylog-api</artifactId>
-      <version>\${tinylog-api.version}</version>
+      <version>${tinylog-api.version}</version>
     </dependency>
     <dependency>
       <groupId>org.tinylog</groupId>
       <artifactId>tinylog-impl</artifactId>
-      <version>\${tinylog-impl.version}</version>
+      <version>${tinylog-impl.version}</version>
     </dependency>
   </dependencies>
 
-  <build>    
+  <build>
+    <resources>
+      <resource>
+        <directory>src/main/resources</directory>
+      </resource>
+    </resources>
     <plugins>
+      <!-- allows to compile and build the program -->
       <plugin>
         <groupId>org.apache.maven.plugins</groupId>
         <artifactId>maven-compiler-plugin</artifactId>
-        <version>\${maven-compiler-plugin.version}</version>
+        <version>${maven-compiler-plugin.version}</version>
       </plugin>
+      <!-- creates an uber-jar file with all
+           dependencies and resources inside -->
       <plugin>
         <groupId>org.apache.maven.plugins</groupId>
-        <artifactId>maven-resources-plugin</artifactId>
-        <version>\${maven-resources-plugin.version}</version>
+        <artifactId>maven-shade-plugin</artifactId>
+        <version>${maven-shade-plugin.version}</version>
         <executions>
           <execution>
-            <id>copy-resources</id>
-            <phase>validate</phase>
-            <goals>
-              <goal>copy-resources</goal>
-            </goals>
-            <configuration>
-              <outputDirectory>/target/src/main/resources</outputDirectory>
-              <includeEmptyDirs>true</includeEmptyDirs>
-              <resources>
-                <resource>
-                  <directory>/src/main/resources</directory>
-                  <filtering>false</filtering>
-                </resource>
-              </resources>
-            </configuration>
-          </execution>
-        </executions>
-      </plugin>
-      <plugin>
-        <groupId>org.apache.maven.plugins</groupId>
-        <artifactId>maven-jar-plugin</artifactId>
-        <version>\${maven-jar-plugin.version}</version>
-        <configuration>
-          <archive>
-            <manifest>
-              <addClasspath>true</addClasspath>
-	          <classpathPrefix>lib/</classpathPrefix> <!-- enables usage of dependencies from .jar
-                                                           by copying them into the target folder -->
-              <mainClass>$firstLevelPackageName.$secondLevelPackageName.$projectName.Main</mainClass>
-            </manifest>
-          </archive>
-        </configuration>
-      </plugin>
-      <plugin>
-        <!-- enables usage of dependencies from .jar
-             by copying them into the target folder -->
-        <groupId>org.apache.maven.plugins</groupId>
-        <artifactId>maven-dependency-plugin</artifactId>
-        <version>\${maven-dependency-plugin.version}</version>
-        <executions>
-          <execution>
-            <id>copy-dependencies</id>
             <phase>package</phase>
             <goals>
-              <goal>copy-dependencies</goal>
+              <goal>shade</goal>
             </goals>
             <configuration>
-              <outputDirectory>\${project.build.directory}/lib</outputDirectory>
+              <transformers>
+                <transformer implementation="org.apache.maven.plugins.shade.resource.ManifestResourceTransformer">
+                  <mainClass>$firstLevelPackageName.$secondLevelPackageName.$projectName.Main</mainClass>
+                </transformer>
+              </transformers>
             </configuration>
           </execution>
         </executions>
       </plugin>
+      <!-- prevents from building if unit tests don't pass -->
       <plugin>
-        <!-- prevents from building if unit tests don't pass -->
         <groupId>org.apache.maven.plugins</groupId>
         <artifactId>maven-surefire-plugin</artifactId>
-        <version>\${maven-surefire-plugin.version}</version>
+        <version>${maven-surefire-plugin.version}</version>
       </plugin>
+      <!-- prevents from building if integration tests don't pass -->
       <plugin>
-        <!-- prevents from building if integration tests don't pass -->
         <groupId>org.apache.maven.plugins</groupId>
         <artifactId>maven-failsafe-plugin</artifactId>
-        <version>\${maven-failsafe-plugin.version}</version>
+        <version>${maven-failsafe-plugin.version}</version>
       </plugin>
+      <!-- creates reports on tests coverage (target->site->jacoco->index.html) -->
       <plugin>
-        <!-- creates reports on tests coverage (target->site->jacoco->index.html) -->
         <groupId>org.jacoco</groupId>
         <artifactId>jacoco-maven-plugin</artifactId>
-        <version>\${jacoco-maven-plugin.version}</version>
+        <version>${jacoco-maven-plugin.version}</version>
         <executions>
           <execution>
             <id>prepare-agent</id>
@@ -342,6 +389,9 @@ cat > $gitignoreFile << EOF
 # All files with .class and .iml extension:
 *.class
 *.iml
+
+# Additional pom.xml generated by maven-shade-plugin
+dependency-reduced-pom.xml
 
 # All files with .log extension + file named 'logs.txt':
 *.log
@@ -491,6 +541,8 @@ verifyIfProjectDirectoryExists $projectDirectory
 createProjectDirectory $projectDirectory
 createFilesStructure $projectDirectory $firstLevelPackageName $secondLevelPackageName $projectName
 insertContentToMain $projectDirectory $firstLevelPackageName $secondLevelPackageName $projectName $gitCommitterName $gitCommitterSurname
+insertContentToSamplePrinter $projectDirectory $firstLevelPackageName $secondLevelPackageName $projectName $gitCommitterName $gitCommitterSurname
+insertContentToSampleLines $projectDirectory
 insertContentToLoggerProperties $projectDirectory
 insertContentToMainTest $projectDirectory $firstLevelPackageName $secondLevelPackageName $projectName
 insertContentToPom $projectDirectory $firstLevelPackageName $secondLevelPackageName $projectName $projectURL
